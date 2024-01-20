98321e8efd87d951ca804ad93f555e0d31d4091c
Samit Uttarkar <samituttarkar@gmail.com>
2024-01-20 21:05:02 +0000

    Data Transformation

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,6 +7,9 @@ from src.logger import logging
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
 # any input required we will give through this class
 @dataclass # we don't need to use __init__ to define our variables using dataclass
 class DataIngestionConfig:
@@ -41,7 +44,24 @@ class DataIngestion:
         except Exception as e:
             raise CustomException(e,sys)
         
+'''This class is responsible for the actual data ingestion process.
+__init__ Method
+Initializes an instance of DataIngestionConfig and stores it in self.ingestion_config. This instance holds the paths for the train, test, and raw data.
+initiate_data_ingestion Method
+Logs the start of the data ingestion process.
+Tries to execute the data ingestion logic inside a try block, which allows for exception handling.
+Reads a dataset from a specified CSV file (stud.csv) into a pandas DataFrame. This part can be modified to read data from various sources like databases.
+Creates necessary directories for storing output files using os.makedirs.
+Saves the raw data into a CSV file specified by self.ingestion_config.raw_data_path.
+Splits the data into training and testing sets using train_test_split from scikit-learn, with a specified test size and random state for reproducibility.
+Saves the split datasets into their respective CSV files as specified in the configuration.
+Logs the completion of the data ingestion process.
+Returns the paths to the train and test data files.'''
+        
 if __name__=="__main__":
     obj=DataIngestion()
     train_data,test_data=obj.initiate_data_ingestion()
+
+    data_transformation = DataTransformation()
+    data_transformation.initiate_data_transformation(train_data,test_data)
         
